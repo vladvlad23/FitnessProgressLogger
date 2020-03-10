4db98af20813700a198066086976cae370387eab
vlad <ungureanu.vlad1@gmail.com>
2020-03-10 16:37:51 +0200

    Updates

---
+++ precache-manifest.1ef3dceeafe0c02164b426a0d6f52513.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffed6ece27be5305d41598d3e966e66",
+    "url": "/vladvlad23/FitnessProgressLogger/index.html"
+  },
+  {
+    "revision": "91ca06805322aa1d9812",
+    "url": "/vladvlad23/FitnessProgressLogger/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "00b1a7b69ecb48894a36",
+    "url": "/vladvlad23/FitnessProgressLogger/static/js/2.53b2ac2e.chunk.js"
+  },
+  {
+    "revision": "91ca06805322aa1d9812",
+    "url": "/vladvlad23/FitnessProgressLogger/static/js/main.f38920aa.chunk.js"
+  },
+  {
+    "revision": "a628a85c53a4b3411975",
+    "url": "/vladvlad23/FitnessProgressLogger/static/js/runtime~main.141c24fb.js"
+  },
+  {
+    "revision": "d9853974b0d315f835e8128391a772ee",
+    "url": "/vladvlad23/FitnessProgressLogger/static/media/deadlifting.d9853974.jpg"
+  },
+  {
+    "revision": "3faa99e4b82b4f203932e4f152b706b0",
+    "url": "/vladvlad23/FitnessProgressLogger/static/media/githubIcon.3faa99e4.svg"
+  },
+  {
+    "revision": "7013e1334e1c9b51d16ee430ce7d6452",
+    "url": "/vladvlad23/FitnessProgressLogger/static/media/overheadpressing.7013e133.jpg"
+  }
+]);
